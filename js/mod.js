8c44aad24f8033acbb51012bcd42c8eb7d522c60
RTXT25 <skoolhumanboi@gmail.com>
2021-10-19 20:46:29 +1100

    all

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Bored Tree",
+	id: "Boredmod",
+	author: "RTXT25",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
@@ -13,14 +13,13 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "it here",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.1</h3><br>
+		i made the thing`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
